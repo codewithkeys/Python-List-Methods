9b5d14d96951e9cac0da0b44f7be3e3bcfa32dc0
Yoly Keys <116820221+codewithkeys@users.noreply.github.com>
2022-12-25 18:58:48 +0000

    Added additional list methods

--- main.py
+++ main.py
@@ -29,3 +29,53 @@ tropical = ["mango", "pineapple", "papaya"]
 thislist.extend(tropical)
 print(thislist)
 
+# Add Any Iterable
+# The extend() method does not have to append lists, you can add any iterable object (tuples, sets, dictionaries etc.).
+# Add elements of a tuple to a list:
+
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+
+# Python - Remove List Items
+# Remove Specified Item
+# The remove() method removes the specified item.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+# Remove Specified Index
+# The pop() method removes the specified index.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+# Note: If you do not specify the index, the pop() method removes the last item.
+# Remove the last item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+# The del keyword also removes the specified index:
+# Remove the first item:
+
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+# The del keyword can also delete the list completely.
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+# Clear the List
+# The clear() method empties the list.
+# The list still remains, but it has no content.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
