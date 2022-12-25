e725ca814a82a25f7bae06f53f2fbf86f212ef44
Yoly Keys <116820221+codewithkeys@users.noreply.github.com>
2022-12-25 18:41:50 +0000

    Added additional methods

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
+# ZTM Exercise - add my new friend
 friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
-
 new_friend = ['Stanley']
-
 friends.extend(new_friend)
-
 print(sorted(friends))
+
+# Append Items
+# To add an item to the end of the list, use the append() method:
+# Using the append() method to append an item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+# Insert Items
+# To insert a list item at a specified index, use the insert() method.
+# The insert() method inserts an item at the specified index:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+# Extend List
+# To append elements from another list to the current list, use the extend() method.
+# Add the elements of tropical to thislist:
+
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
